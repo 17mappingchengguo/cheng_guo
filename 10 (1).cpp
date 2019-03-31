3186fc59aa7a496189ae79eaedb08c47243a3920
DESKTOP-PTCE28C\Y <2186479577@qq.com>
2019-03-31 18:34:05 +0800

    17测绘成果

---
+++ 10 (1).cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+#include <Polynomial.h>
+
+using namespace std;
+
+#include<string>
+
+
+
+int main()
+
+{
+
+   Polynomial a(4);
+
+   cout<<a;
+
+   cin>>a;
+
+   cout<<a;
+
+   Polynomial b(4);
+
+   cin>>b;
+
+   cout<<a+b;
+
+}
